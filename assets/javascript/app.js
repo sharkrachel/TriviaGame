ef26e661b4cb0e1553ae4e016116f78bc9379140
Rachel Sipes <rasipes@gmail.com>
2020-01-01 18:56:34 -0500

    added psuedocode to js file and started html file

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// PSUEDOCODE
+
+// The game initially starts by pressing the Start Button
+// Start a 30 second countdown clock - perhaps the getTime(). https://www.w3schools.com/howto/howto_js_countdown.asp
+// At the same time, display one question
+// At the same time, display multiple choice answers
+// If correct answer is chosen, show a message saying "correct" and an image or gif
+// If wrong answer is chosen, show a message saying "incorrect: and show an image or gif of the correct answer with text "the correct answer was...:"
+// If time runs up, show "time is up" and show an image or gif of the correct answer with text "the correct answer was...:"
+// After answer is shown (in any instance) wait 5 seconds and automatically show the next question
+// Once all questions are answered, display scoreboard and a "Start Over" button 
+// If "Start Over" button is clicked, a new game starts 
+// At no point should a user refresh the page
+// Thats all, easy, right?
+
