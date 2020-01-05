3d75326c3632e3966efdb0d29137e68069e44135
Rachel Sipes <rasipes@gmail.com>
2020-01-05 14:20:39 -0500

    working start button, timer, first question and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,133 @@
 // PSUEDOCODE
 
+// User can start the quiz
+// User can see question
+// User can see time remaining
+// User can answer right
+// User can answer wrong
+// User can not answer and run out of time
+// User can complete quiz
+// User can see total right
+// User can see total wrong
+
 // The game initially starts by pressing the Start Button
 // Start a 30 second countdown clock - perhaps the getTime(). https://www.w3schools.com/howto/howto_js_countdown.asp
-window.onload = function() {
-$(".start").on("click", start)
+
+//setInterval()
+
+var time = 10;
+var intervalId;
+var currentQuestion = 0;
+var right = 0;
+var wrong = 0;
+
+var questions = [
+    {
+        questionText: "What is a function inside of an object called?",
+        answers: ["Method", "Object Function", "Array", "While Loop"],
+        correctAnswer: 0
+    },
+    {
+        questionText: "What is the correct way to write an array?",
+        answers: ["var fruit = ('apple', 'pear', 'banana')", "var fruit = [apple, pear, banana]", "var fruit = 'apple', 'pear', 'banana'", "var fruit = ['apple', 'pear', 'banana']"],
+        correctAnswer: 3
+    },
+    {
+        questionText: "Which function returns a random number?",
+        answers: ["math.random()", "Math.floor()", "Math.random()", "math.Random()"],
+        correctAnswer: 2
+    },
+    {
+        questionText: "JavaScript and Java are the same thing?",
+        answers: ["True", "Fasle"],
+        correctAnswer: 1
+    }
+]
+
+function startGame() {
+    $(".start").click(function () {
+        $(".timer").append(time);
+        $(".start").hide();
+        resetTimer();
+        displayQuestion(currentQuestion);
+    });
+
 }
 
-var time = 25;
-$(".timer").text(time);
+function resetTimer() {
+    time = 10;
+    window.clearInterval(intervalId);
+    intervalId = window.setInterval(onTimeChange, 1000);
+}
 
-function reset() {
-    time = 25;
+function onTimeChange() {
+    if (time >= 0) {
+        updateTimeDisplay(time - 1);
+    }
+    else {
+        outOfTime();
+    }
+
+}
+
+function updateTimeDisplay(newTime) {
     $(".timer").text(time);
+    time = newTime;
 }
+
+function outOfTime() {
+    $(".questions").hide();
+    $(".answers").hide();
+    $(".times-up").text("Out of time");
+    resetTimer();
+    nextQuestion();
+    window.setTimeout(displayQuestion, 3000, currentQuestion);
+    // displayQuestion(currentQuestion);
+}
+
+function displayQuestion(id) {
+    var question = questions[id];
+    var text = question.questionText;
+
+    $(".questions").append("<p>" + text + "</p>");
+
+    var answers = question.answers;
+    for (var i = 0; i < answers.length; i++) {
+        $(".answers").append("<button value=" + i + ">" + answers[i] + "</button>");
+    }
+    $(".answers button").click(handleAnswerClick);
+}
+
+
+function nextQuestion() {
+    if (currentQuestion < questions.length - 1) {
+        currentQuestion++
+    }
+}
+
+function handleAnswerClick(event) {
+    userGuess = event.target.value;
+    $(".questions").hide();
+    $(".answers").hide();
+
+    if (userGuess === questions.correctAnswer) {
+        $(".correct").text("You are correct!");
+        $("image").append("<img src='assets/images/correct.png'>");
+        right++;
+    }
+
+    else (userGuess != questions.correctAnswer) {
+        $(".incorrect").text("You are incorrect");
+        $("image").append("<img src='assets/images/incorrect.png'>");
+        wrong++;
+    }
+}
+
+
+
+startGame();
+
+
 
 // At the same time, display one question
 // At the same time, display multiple choice answers
